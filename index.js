704189af0d3055e0d7a6a6b9336059910a9f63e4
Rohitjadhav19899 <122199406+Rohitjadhav19899@users.noreply.github.com>
2023-01-21 23:18:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+
+let password1 = document.getElementById("password_1")
+let password2 = document.getElementById("password_2")
+
+function generatepass() {
+
+    password1.textContent = ''
+    password2.textContent = ''
+
+    for (let i = 0; i < 15; i++) {
+        let randomNum = Math.floor(Math.random() * 60) + 1
+        password1.textContent += characters[randomNum]
+        randomNum = Math.floor(Math.random() * 60) + 1
+        password2.textContent += characters[randomNum]
+    }
+}
